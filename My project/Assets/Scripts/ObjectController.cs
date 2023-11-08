301b8fb630c2b15ccb890ef6f8ce0780765c1c69
MitchZas <mitchellzasa@gmail.com>
2023-11-08 10:09:06 -0500

    Interact System

---
+++ My project/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectController : MonoBehaviour
+{
+  [SerializeField] private string itemName;
+
+  [TextArea] [SerializeField] private string itemextraInfo;
+
+  [SerializeField] private InspectController inspectController;
+
+  public void ShowObjectName()
+  {
+        inspectController.ShowName(itemName);
+  }
+
+  public void HideObjectName()
+  {
+        inspectController.HideName();
+  }
+
+  public void ShowExtraInfo()
+  {
+    inspectController.ShowAdditionalInfo(itemextraInfo);
+  }
+}
